499f09af9b39db9da5bf21043680d6c30514f10f
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2020-05-19 13:14:32 -0300

    Update and rename 8 to 8.java

---
+++ 8)/8.java
@@ -0,0 +1,38 @@
+package atividades;
+public class Guitarra {
+    String modelo;
+    int cordas;
+    String cor;
+    String marca;
+    String afinacao;
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public int getCordas() {
+        return cordas;
+    }
+    public void setCordas(int cordas) {
+        this.cordas = cordas;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getAfinacao() {
+        return afinacao;
+    }
+    public void setAfinacao(String afinacao) {
+        this.afinacao = afinacao;
+    }
+}
